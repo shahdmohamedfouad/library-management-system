8e727f3c1cab13b2cea493f1d3003a21308e7aa2
Shahd Mohamed Fouad <shahdmohamedfouad270@gmail.com>
2026-05-14 09:24:17 +0300

    Update user_schema.py

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,1 +1,19 @@
+# app/schemas/user.py
+from pydantic import BaseModel, EmailStr
+from typing import Optional
 
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
